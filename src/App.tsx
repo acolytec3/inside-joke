8283f4215ef2dc9bbc7056368d055dce653116f9
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-01-08 06:15:32 -0500

    Update metadata

--- src/App.tsx
+++ src/App.tsx
@@ -42,8 +42,8 @@ const options = {
   },
   addresses: {
     listen: [
-      //  "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
-      //  "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
+     //   "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
+     //   "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
       "/ip4/127.0.0.1/tcp/13579/wss/p2p-webrtc-star",
     ],
   },
